43e9b37b2a2a768cf76eaf394fcf862acd2a86f1
otani <gaokanoyakata@gmail.com>
2023-08-24 02:56:03 +0000

    課題9提出

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
+
 //課題内容
+Route::controller(AAAController::class)->group(function() {
+    Route::get('XXX', 'bbb');
+});
